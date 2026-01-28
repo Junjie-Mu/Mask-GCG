df889cd18b7d60ff4c6df8d9b6befacccbf67225
Junjie-Mu <junjie.mu@mail.polimi.it>
2026-01-28 20:35:44 +0100

    update

--- llm_attacks/minimal_gcg/opt_utils.py
+++ llm_attacks/minimal_gcg/opt_utils.py
@@ -9,29 +9,6 @@ from llm_attacks import get_embedding_matrix, get_embeddings
 
 
 def token_gradients(model, input_ids, input_slice, target_slice, loss_slice):
-
-    """
-    Computes gradients of the loss with respect to the coordinates.
-    
-    Parameters
-    ----------
-    model : Transformer Model
-        The transformer model to be used.
-    input_ids : torch.Tensor
-        The input sequence in the form of token ids.
-    input_slice : slice
-        The slice of the input sequence for which gradients need to be computed.
-    target_slice : slice
-        The slice of the input sequence to be used as targets.
-    loss_slice : slice
-        The slice of the logits to be used for computing the loss.
-
-    Returns
-    -------
-    torch.Tensor
-        The gradients of each token in the input_slice with respect to the loss.
-    """
-
     embed_weights = get_embedding_matrix(model)
     one_hot = torch.zeros(
         input_ids[input_slice].shape[0],
